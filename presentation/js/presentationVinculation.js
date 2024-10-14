325df96c1ff8348c1fc8240c835d838ed866576e
Michael Guzmán <116075308+AntonioNoguera@users.noreply.github.com>
2024-10-14 14:40:44 -0600

    Feat [ Hyperlink basic navigations ] -> User now is able to navigate beetwen screens as its supposed

--- presentation/js/presentationVinculation.js
+++ presentation/js/presentationVinculation.js
@@ -1,9 +1,16 @@
 import { loginTest } from './app_validations/auth_validations.js';
 
 const Routes = Object.freeze({
+    //NODOS DE RUTA DISPONIBLE
+
     REGISTER_PAGE: '/html/auth/register_page.html',
     LOGIN_PAGE: '/html/auth/index.html',
-    // otros routes
+
+    FILES_PAGE: '../../html/merge_files/files_page.html',
+    HISTORY_PAGE: '../../html/merge_files/history_page.html',
+    MERGE_PAGE: '../../html/merge_files/merge_page.html',
+
+    ADMIN_PAGE: '../../html/user_administration/user_admin_page.html',
 });
 
 function freeNavigateTo(data) { 
@@ -12,13 +19,46 @@ function freeNavigateTo(data) {
 
 function navigateTo (data, content) {
     switch (data) {
+        
         case Routes.REGISTER_PAGE:
-            console.log("ENtenr")
+            
             if(loginTest(content)){
                 freeNavigateTo(Routes.REGISTER_PAGE);
             }
-            break;  // No olvides los breaks en los cases
-        default:
+
+            break;
+
+        case Routes.LOGIN_PAGE:
+             
+            freeNavigateTo(Routes.LOGIN_PAGE); 
+
+            break;
+
+        case Routes.FILES_PAGE:
+             
+            freeNavigateTo(Routes.FILES_PAGE); 
+
+            break; 
+        
+        case Routes.HISTORY_PAGE:
+                
+            freeNavigateTo(Routes.HISTORY_PAGE); 
+
+            break;
+
+        case Routes.MERGE_PAGE:
+             
+            freeNavigateTo(Routes.MERGE_PAGE); 
+
+            break;
+
+        case Routes.ADMIN_PAGE:
+             
+            freeNavigateTo(Routes.ADMIN_PAGE); 
+
+            break;
+    
+    default:
             console.log("Not in a case");
     }
 }
